baac74c00f71322eadeb0a1b2055e2ab8609fa2d
herewithme <amaury@beapi.fr>
2010-08-23 09:06:20 +0000

    call correct function

--- inc/functions.hook.php
+++ inc/functions.hook.php
@@ -8,6 +8,6 @@
  * @return boolean
  */
 function remove_meta_during_delete( $term = null, $tt_id = 0, $taxonomy = '' ) {
-	return delete_term_meta_by_term_taxonomy_id( $tt_id );
+	return delete_term_taxo_by_term_taxonomy_id( $tt_id );
 }
 ?>
