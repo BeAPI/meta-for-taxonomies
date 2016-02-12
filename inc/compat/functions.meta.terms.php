c883b452906012d09987ee894dcf455d81bfb9bb
Nicolas Juen <njuen@beapi.fr>
2016-02-12 16:28:39 +0100

    Add deprecated argument

--- inc/compat/functions.meta.terms.php
+++ inc/compat/functions.meta.terms.php
@@ -1,127 +1,21 @@
 <?php
 /**
- * Add metadata for term
- *
- * @param string $taxonomy 
- * @param integer $term_id 
- * @param string $meta_key 
- * @param string|array $meta_value 
- * @param boolean $unique 
- * @return boolean
- * @author Amaury Balmer
- */
-/*
-function add_term_meta( $taxonomy = '', $term_id = 0, $meta_key = '', $meta_value = '', $unique = false ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
-	}
-	
-	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
-		return false;
-	}
-	
-	return add_term_taxonomy_meta( $term->term_taxonomy_id, $meta_key, $meta_value, $unique );
-}*/
-
-/**
- * Delete term meta for term
- *
- * @param string $taxonomy 
- * @param integer $term_id 
- * @param string $meta_key 
- * @param string|array $meta_value 
- * @return boolean
- * @author Amaury Balmer
- */
-/*
-function delete_term_meta( $taxonomy = '', $term_id = 0, $meta_key = '', $meta_value = '') {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
-	}
-	
-	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
-		return false;
-	}
-	
-	return delete_term_taxonomy_meta( $term->term_taxonomy_id, $meta_key, $meta_value );
-}*/
-
-/**
  * Get a term meta field
  *
- * @param string $taxonomy 
- * @param integer $term_id 
- * @param string|array $meta_key 
- * @param boolean $single 
- * @return boolean
- * @author Amaury Balmer
- */
-/*
-function get_term_meta( $taxonomy = '', $term_id = 0, $meta_key = '', $single = false ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
-	}
-	
-	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
-		return false;
-	}
-	
-	return get_term_taxonomy_meta( $term->term_taxonomy_id, $meta_key, $single );
-}*/
-
-/**
- * Update a term meta field
- *
- * @param string $taxonomy 
- * @param integer $term_id 
- * @param string $meta_key 
- * @param string|array $meta_value 
- * @param string|array $prev_value 
- * @return boolean
- * @author Amaury Balmer
- */
-/*
-function update_term_meta( $taxonomy = '', $term_id = 0, $meta_key, $meta_value, $prev_value = '' ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
-	}
-	
-	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
-		return false;
-	}
-	
-	return update_term_taxonomy_meta( $term->term_taxonomy_id, $meta_key, $meta_value, $prev_value );
-}*/
-
-/**
- * Get a term meta field
- *
- * @param string $taxonomy 
- * @param integer $term_id 
+ * @param string $taxonomy deprecated since 1.3.0
+ * @param integer $term_id
  * @return boolean
  * @author Amaury Balmer
  */
 function get_term_custom( $taxonomy = '', $term_id = 0 ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
+
+	if ( ! empty( $taxonomy ) ) {
+		_deprecated_argument( 'get_term_custom', '1.3.0', sprintf( __( 'The %s parameter is no longer needed.' ), 'taxonomy' ) );
 	}
-	
+
 	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
+	$term = get_term( (int) $term_id );
+	if ( false === $term || is_wp_error($term) ) {
 		return false;
 	}
 
@@ -131,20 +25,22 @@ function get_term_custom( $taxonomy = '', $term_id = 0 ) {
 /**
  * undocumented function
  *
- * @param string $taxonomy 
- * @param string $term_id 
- * @return void
+ * @param string $taxonomy deprecated since 1.3.0
+ * @param string $term_id
+ *
+ * @return array|bool|null
+ *
  * @author Amaury Balmer
  */
 function get_term_custom_keys( $taxonomy = '', $term_id = 0 ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
+
+	if ( ! empty( $taxonomy ) ) {
+		_deprecated_argument( 'get_term_custom_keys', '1.3.0', sprintf( __( 'The %s parameter is no longer needed.' ), 'taxonomy' ) );
 	}
-	
+
 	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
+	$term = get_term( (int) $term_id );
+	if ( false === $term || is_wp_error($term) ) {
 		return false;
 	}
 
@@ -154,21 +50,23 @@ function get_term_custom_keys( $taxonomy = '', $term_id = 0 ) {
 /**
  * undocumented function
  *
- * @param string $taxonomy 
- * @param string $term_id 
- * @param string $key 
- * @return void
+ * @param string $taxonomy
+ * @param string $term_id deprecated since 1.3.0
+ * @param string $key
+ *
+ * @return array|bool
+ *
  * @author Amaury Balmer
  */
 function get_term_custom_values( $taxonomy = '', $term_id = 0, $key = '' ) {
-	// Taxonomy is valid ?
-	if ( !taxonomy_exists($taxonomy) ) {
-		return false;
+
+	if ( ! empty( $taxonomy ) ) {
+		_deprecated_argument( 'get_term_custom_values', '1.3.0', sprintf( __( 'The %s parameter is no longer needed.' ), 'taxonomy' ) );
 	}
-	
+
 	// Term ID exist for this taxonomy ?
-	$term = get_term( (int) $term_id, $taxonomy );
-	if ( $term == false || is_wp_error($term) ) {
+	$term = get_term( (int) $term_id );
+	if ( false === $term || is_wp_error($term) ) {
 		return false;
 	}
 
