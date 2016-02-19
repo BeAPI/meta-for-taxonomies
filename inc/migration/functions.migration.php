27e69e773e7897cfd081ae9be81e42d02127c4fb
Clement Boirie <cboirie@beapi.fr>
2016-02-19 19:14:25 +0100

    add basic structure for migration script

--- inc/migration/functions.migration.php
+++ inc/migration/functions.migration.php
@@ -1,5 +1,6 @@
 <?php
 
+add_action( 'mft_migrate_term_metas_batch', '_mft_batch_migrate_terms_metas' );
 /**
  * Migrate existing meta terms to the new WordPress table
  *
@@ -9,6 +10,55 @@ function _mft_batch_migrate_terms_metas() {
 
 	// Ensure our table is register
 	_mft_maybe_register_taxometa_table();
+
+	global $wpdb;
+
+	$lock_name = 'mft_term_metas.lock';
+
+	// Try to lock.
+	$lock_result = $wpdb->query( $wpdb->prepare( "INSERT IGNORE INTO `$wpdb->options` ( `option_name`, `option_value`, `autoload` ) VALUES (%s, %s, 'no') /* LOCK */", $lock_name, time() ) );
+
+	if ( ! $lock_result ) {
+		$lock_result = get_option( $lock_name );
+
+		// Bail if we were unable to create a lock, or if the existing lock is still valid.
+		if ( ! $lock_result || ( $lock_result > ( time() - HOUR_IN_SECONDS ) ) ) {
+			wp_schedule_single_event( time() + ( 5 * MINUTE_IN_SECONDS ), 'mft_migrate_term_metas_batch' );
+
+			return;
+		}
+	}
+
+	// Update the lock, as by this point we've definitely got a lock, just need to fire the actions.
+	update_option( $lock_name, time() );
+
+
+	/**
+	 * 1) Recuperer les 10 prochain term metas avec le nouveau t_id
+	 * 2) insérer la donnée dans la nouvelle table
+	 * 3) supprimer la donnée de l'ancienne table (delete_metadata)
+	 */
+
+	// Get a list of shared terms (those with more than one associated row in term_taxonomy).
+	$shared_terms = $wpdb->get_results(
+		"SELECT tt.term_id, t.*, count(*) as term_tt_count FROM {$wpdb->term_taxonomy} tt
+		 LEFT JOIN {$wpdb->terms} t ON t.term_id = tt.term_id
+		 GROUP BY t.term_id
+		 HAVING term_tt_count > 1
+		 LIMIT 10"
+	);
+
+	// No more terms, we're done here.
+	if ( ! $shared_terms ) {
+		update_option( 'finished_migrating_terms_metas', true );
+		delete_option( $lock_name );
+		return;
+	}
+
+	// Shared terms found? We'll need to run this script again.
+	wp_schedule_single_event( time() + ( 2 * MINUTE_IN_SECONDS ), 'mft_migrate_term_metas_batch' );
+
+	delete_option( $lock_name );
 }
 
 /**
--- inc/migration/migration.php
+++ inc/migration/migration.php
@@ -3,3 +3,22 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
+
+/**
+ * Bootstrap terms metas migration.
+ */
+
+$finished_split_terms = get_option( 'finished_splitting_shared_terms', false );
+$finished_migrating_terms_metas = get_option( 'finished_migrating_terms_metas', false );
+
+// We have to wait for wp split terms completion
+if ( ! $finished_split_terms || $finished_migrating_terms_metas ) {
+	return;
+}
+
+// Avoid rescheduling our cron
+if ( ! $finished_migrating_terms_metas && wp_next_scheduled( 'mft_migrate_term_metas_batch' ) ) {
+	return;
+}
+
+wp_schedule_single_event( time() + MINUTE_IN_SECONDS, 'mft_migrate_term_metas_batch' );
