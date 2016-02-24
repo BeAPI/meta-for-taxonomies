983f9e305ed721f0a4231d85db417980b2efc701
Clement Boirie <cboirie@beapi.fr>
2016-02-24 21:38:13 +0100

    migrate metas to the new table

--- inc/migration/functions.migration.php
+++ inc/migration/functions.migration.php
@@ -3,6 +3,7 @@
 add_action( 'mft_migrate_term_metas_batch', '_mft_batch_migrate_terms_metas' );
 /**
  * Migrate existing meta terms to the new WordPress table
+ * Heavily inspired by _wp_batch_split_terms
  *
  * @author Clément Boirie
  */
@@ -40,23 +41,64 @@ function _mft_batch_migrate_terms_metas() {
 	 */
 
 	// Get a list of shared terms (those with more than one associated row in term_taxonomy).
-	$shared_terms = $wpdb->get_results(
-		"SELECT tt.term_id, t.*, count(*) as term_tt_count FROM {$wpdb->term_taxonomy} tt
-		 LEFT JOIN {$wpdb->terms} t ON t.term_id = tt.term_id
-		 GROUP BY t.term_id
-		 HAVING term_tt_count > 1
-		 LIMIT 10"
+	$terms_metas = $wpdb->get_results(
+		"SELECT ttm.meta_id, tt.taxonomy, tt.term_id, ttm.meta_key, ttm.meta_value
+		 FROM {$wpdb->term_taxometa} ttm
+		 INNER JOIN {$wpdb->term_taxonomy} tt
+		 ON tt.term_taxonomy_id = ttm.term_taxo_id
+		 ORDER BY ttm.meta_id
+		 LIMIT 10;"
 	);
 
 	// No more terms, we're done here.
-	if ( ! $shared_terms ) {
+	if ( ! $terms_metas ) {
 		update_option( 'finished_migrating_terms_metas', true );
 		delete_option( $lock_name );
 		return;
 	}
 
-	// Shared terms found? We'll need to run this script again.
+	// Terms metas found? We'll need to run this script again.
 	wp_schedule_single_event( time() + ( 2 * MINUTE_IN_SECONDS ), 'mft_migrate_term_metas_batch' );
+
+	$failed_transactions = get_option( 'mft_migrate_fails', array() );
+	$previous_failed_transactions_count = count( $failed_transactions );
+
+	// Insert metas to the wordpress metas table
+	foreach( $terms_metas as $meta ) {
+
+		$update = update_term_meta( $meta->taxonomy, $meta->term_id, $meta->meta_key, $meta->meta_value );
+
+		// If something went wrong save the term metas data and continue
+		if ( is_wp_error( $update ) || false === $update ) {
+			$oops = array(
+				'taxonomy' => $meta->taxonomy,
+				'term_id' => $meta->term_id,
+				'meta_key' => $meta->meta_key,
+				'meta_value' => $meta->meta_value,
+				'is_wp_error' => is_wp_error( $update ),
+			);
+
+			$failed_transactions[] = $oops;
+		}
+	}
+
+	// Save failed transactions if new ones
+	if ( count( $failed_transactions ) !== $previous_failed_transactions_count ) {
+		update_option( 'mft_migrate_fails', $failed_transactions );
+	}
+
+	// Build an array of old meta ids and delete them
+	$ids = array_filter( array_map( 'absint', wp_list_pluck( $terms_metas, 'meta_id' ) ) );
+
+	$wpdb->query(
+		$wpdb->prepare(
+			"
+			DELETE FROM {$wpdb->term_taxometa}
+			WHERE meta_id IN ( %s )
+			",
+			implode( ',', $ids )
+		)
+	);
 
 	delete_option( $lock_name );
 }
