4971c002dfccd3fb47851203e109e3e900aa68e1
Nicolas Juen <njuen@beapi.fr>
2016-03-14 15:11:44 +0100

    Quit if finisehd

--- inc/migration/migration.php
+++ inc/migration/migration.php
@@ -13,7 +13,7 @@ class MFT_Migration {
 	 * MFT_Migration constructor.
 	 */
 	public function __construct() {
-		if( ! self::is_finished() ) {
+		if( self::is_finished() ) {
 			return;
 		}
 
@@ -31,11 +31,11 @@ class MFT_Migration {
 		/**
 		 * Bootstrap terms metas migration.
 		 */
-		$finished_split_terms = get_option( 'finished_splitting_shared_terms', false );
-		$finished_migrating_terms_metas = get_option( 'finished_migrating_terms_metas', false );
+		$finished_split_terms = (bool)get_option( 'finished_splitting_shared_terms', false );
+		$finished_migrating_terms_metas = (bool)get_option( 'finished_migrating_terms_metas', false );
 
 		// We have to wait for wp split terms completion
-		if ( ! $finished_split_terms || ! $finished_migrating_terms_metas ) {
+		if ( false === $finished_split_terms || false === $finished_migrating_terms_metas ) {
 			return false;
 		}
 
@@ -43,7 +43,7 @@ class MFT_Migration {
 	}
 
 	public static function can_launch_next() {
-		$finished_migrating_terms_metas = get_option( 'finished_migrating_terms_metas', false );
+		$finished_migrating_terms_metas = (bool)get_option( 'finished_migrating_terms_metas', false );
 		// Avoid rescheduling our cron
 		if ( ! $finished_migrating_terms_metas && wp_next_scheduled( 'mft_migrate_term_metas_batch' ) ) {
 			return false;
