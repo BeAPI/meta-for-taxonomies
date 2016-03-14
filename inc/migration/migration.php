08d60635757b1a016430c4b60aebb0497d9f11d0
Nicolas Juen <njuen@beapi.fr>
2016-03-14 16:35:13 +0100

    Put on static to be easily removed

--- inc/migration/migration.php
+++ inc/migration/migration.php
@@ -21,8 +21,7 @@ class MFT_Migration {
 		 * Handle the possibility of not having finished the job but you need to fully return the
 		 * values of the term metas.
 		 */
-		add_filter( 'get_term_metadata', array( $this, 'get_term_metadata' ), 10, 4 );
-
+		add_filter( 'get_term_metadata', array( __CLASS__, 'get_term_metadata' ), 10, 4 );
 
 		if( ! self::can_launch_next() ) {
 			return;
@@ -44,7 +43,7 @@ class MFT_Migration {
 	 *
 	 * @return mixed|null
 	 */
-	public function get_term_metadata( $value, $object_id, $meta_key, $single ) {
+	public static function get_term_metadata( $value, $object_id, $meta_key, $single ) {
 		_mft_maybe_register_taxometa_table();
 		$backcompat = get_metadata( 'term_taxo', $object_id, $meta_key, $single );
 		return empty( $backcompat ) ? null : $backcompat;
@@ -68,7 +67,7 @@ class MFT_Migration {
 	public static function can_launch_next() {
 		$finished_migrating_terms_metas = (bool)get_option( 'finished_migrating_terms_metas', false );
 		// Avoid rescheduling our cron
-		if ( ! $finished_migrating_terms_metas && wp_next_scheduled( 'mft_migrate_term_metas_batch' ) ) {
+		if ( true === $finished_migrating_terms_metas && wp_next_scheduled( 'mft_migrate_term_metas_batch' ) ) {
 			return false;
 		}
 
